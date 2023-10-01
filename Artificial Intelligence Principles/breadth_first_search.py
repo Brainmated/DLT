7202a4de37b6c529fdb96669a243a12d8e98d99b
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-01 19:35:01 +0300

    Update breadth_first_search.py

--- Artificial Intelligence Principles/breadth_first_search.py
+++ Artificial Intelligence Principles/breadth_first_search.py
@@ -1,3 +1,4 @@
+import time
 #dictionary to represent the graph, each key corresponds to a node, each value corresponds to a neighbor
 graph = {
     'A': ["B", "C"],
@@ -9,7 +10,7 @@ graph = {
     'G': [], #this node doesnt exist in the graph
     'H': []
 }
-
+start_time = time.time()
 visited = [] #append in this list each visited node
 queue = [] #add each visited node in queue, then remove node when no more neighboring nodes
 
@@ -29,3 +30,6 @@ def breadth_first_search(graph, visited, node): #graph takes the graph created, 
                 queue.append(neighbor) #adds them to the queue list
 
 breadth_first_search(graph, visited, 'A') #starting node set
+end_time = time.time()
+total_time = end_time - start_time
+print("\nCompile Time: ", total_time)
