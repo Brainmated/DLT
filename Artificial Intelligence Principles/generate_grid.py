dd9cc1d8766d835f706aa62dfab05535c821c4a2
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-03 19:38:24 +0300

    Update generate_grid.py

--- Artificial Intelligence Principles/generate_grid.py
+++ Artificial Intelligence Principles/generate_grid.py
@@ -38,19 +38,37 @@ def set_one(grid):
 def set_two(grid, one_pos):
     row_index = random.randint(0, 2)
     col_index = random.randint(0, 2)
-
+    two_pos = [row_index, col_index]
+    #check if the cell is occupied by '1'
     while [row_index, col_index] == one_pos:
         row_index = random.randint(0, 2)
         col_index = random.randint(0, 2)
 
     grid[row_index, col_index] = 2
 
+    return two_pos
+
+def set_three(grid, two_pos):
+    row_index = random.randint(0, 2)
+    col_index = random.randint(0, 2)
+    three_pos = [row_index, col_index]
+    #check if the cell is occupied by '2'
+    while [row_index, col_index] == two_pos:
+        row_index = random.randint(0, 2)
+        col_index = random.randint(0, 2)
+
+    grid[row_index, col_index] = 3
+
     return grid
 
-grid = set_grid()
 def generate_grid():
-    set_one(grid)
-    set_two(grid)
+    grid = set_grid()
+    one_pos = set_one(grid)
+    two_pos = set_two(grid)
+    set_three(grid, two_pos)
 
-generate_grid()
+    return grid
+
+grid = generate_grid()
+print(grid)
 
