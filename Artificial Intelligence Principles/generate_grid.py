ed77ae9219dc2409cfa6f0a9a7ec42bd032ce3a2
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-03 19:05:03 +0300

    Update generate_grid.py

--- Artificial Intelligence Principles/generate_grid.py
+++ Artificial Intelligence Principles/generate_grid.py
@@ -12,7 +12,16 @@ def generate_grid():
 
     grid = np.zeros((height, width), dtype = int)
 
+    return grid
+
+#set 1 in a random position, then modify the method to
+#generate 1 in a different row and col of the initial random position
+def set_ones(grid):
+    for row in range(0, 3):
+        for col in range(0, 3):
+            grid[row][col] = 1
+
     print(grid)
 
-def set_ones():
-    
+grid = generate_grid()
+set_ones(grid)
