67a9e462345ee8dde540f943665a742b78c5515f
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-03 19:32:31 +0300

    Update generate_grid.py

--- Artificial Intelligence Principles/generate_grid.py
+++ Artificial Intelligence Principles/generate_grid.py
@@ -17,20 +17,35 @@ def set_grid():
 #set 1 in a random position, then modify the method to
 #generate 1 in a different row and col of the initial random position
 def set_one(grid):
+
     row_index = random.randint(0, 2)
     col_index = random.randint(0, 2)
+    one_pos = [row_index, col_index]
+
+    '''
+    Search the grid for values of 0
+    row_indice, col_indice = np.where(grid == 0)
+
+    if len(row_indice) == 0:
+        return grid
+    '''
 
     grid[row_index, col_index] = 1
 
-    return grid
+    #return the row and col position of 1
+    return one_pos
 
-def set_two(grid):
+def set_two(grid, one_pos):
     row_index = random.randint(0, 2)
     col_index = random.randint(0, 2)
 
+    while [row_index, col_index] == one_pos:
+        row_index = random.randint(0, 2)
+        col_index = random.randint(0, 2)
+
     grid[row_index, col_index] = 2
 
-    print(grid)
+    return grid
 
 grid = set_grid()
 def generate_grid():
