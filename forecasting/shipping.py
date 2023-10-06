36cf3d495f800a00c5695c389ac0855bfa4f8acc
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-06 19:27:37 +0300

    Update shipping.py

--- forecasting/shipping.py
+++ forecasting/shipping.py
@@ -3,4 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import nltk
 
+data = pd.read_csv("E:/Programming in Python/data/World_Port_Index")
