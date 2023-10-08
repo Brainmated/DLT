6ee0407a51e2834b8d98fd92c61f03d987ac6739
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-08 19:44:19 +0300

    Update neural_network.py

--- Artificial Intelligence Principles/neural_network.py
+++ Artificial Intelligence Principles/neural_network.py
@@ -7,12 +7,28 @@ import time
 
 start_time = time.time()
 data = pd.read_csv("E:/Programming in Python/data/World_Port_Index.csv", header=1)
+print("CSV Read.")
 
+#x and y coordinates in 1st and 2nd column
 x_coords = data.iloc[:, 0].values
-y_coords = data.iloc[:, 0].values
+y_coords = data.iloc[:, 1].values
 
+#coordinates
 coords = np.column_stack((x_coords, y_coords))
+ports = data["PORT_NAME"]
 
-end_time = time.time()
-comp_time = end_time - start_time
-print("Time to read csv: ", comp_time)
+dist = np.zeros((len(ports), len(ports)))
+for i in range(len(ports)):
+    for j in range(len(ports)):
+        #Haversine Formula
+        '''
+        Parameters:
+        lat1 (float): Latitude of the first point in degrees.
+        lon1 (float): Longitude of the first point in degrees.
+        lat2 (float): Latitude of the second point in degrees.
+        lon2 (float): Longitude of the second point in degrees.
+    
+        Returns:
+        float: Distance between the two points in kilometers.
+        '''
+        
