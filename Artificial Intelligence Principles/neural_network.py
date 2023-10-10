21102643853c907f6b54b98bfdae366b00fcfee3
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-10 14:46:15 +0300

    Update neural_network.py

--- Artificial Intelligence Principles/neural_network.py
+++ Artificial Intelligence Principles/neural_network.py
@@ -7,7 +7,7 @@ import time
 import matplotlib.pyplot as plt
 
 start_time = time.time()
-data = pd.read_csv("E:/Programming in Python/data/World_Port_Index.csv", header=1)
+data = pd.read_csv("D:/Programming in Python/data/World_Port_Index.csv", header=0)
 print("CSV Read.")
 
 #x and y coordinates in 1st and 2nd column
@@ -16,40 +16,18 @@ y_coords = data.iloc[:, 1].values
 
 #coordinates
 coords = np.column_stack((x_coords, y_coords))
-ports = data["PORT_NAME"]
+ports = data.iloc[:, 3].values
 
 radius = 6371
 
+port1 = input("Enter Port 1: ")
+port2 = input("Enter Port 2: ")
 
-dist = np.zeros((len(ports), len(ports)))
-for i in range(len(ports)):
-    for j in range(len(ports)):
-        #Haversine Formula
-        return "test"
-    return "test1"
-        
-
-def calculate_distance():
-    '''
-        Parameters:
-        lat1 (float): Latitude of the first point in degrees.
-        lon1 (float): Longitude of the first point in degrees.
-        lat2 (float): Latitude of the second point in degrees.
-        lon2 (float): Longitude of the second point in degrees.
+if all(port in ports for port in ports[port1, port2]):
+    calculate_distance(port1, port2)
+else:
     
-        Returns:
-        float: Distance between the two points in kilometers.
 
-        LAT_DEG = latitude degrees
-        LAT_MIN = latitude minutes
-        LAT_HEMI = latitude hemisphere, N and S for the halves of the hemisphere
-        LONG_DEG = longitude degrees
-        LONG_MIN = longitude minutes
-        LONG_HEMI longitude hemisphere, Eastern and Western Hemisphere
-        '''
-    return 1
-
-x = long1 -long2
 
 def generate_plot():
     plt.scatter(coords[:, 0], coords[:, 1], marker = "o", color = "blue")
