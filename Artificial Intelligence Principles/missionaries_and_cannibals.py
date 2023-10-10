ff396d774a3e70d2fc01578a4d289af11d73f580
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-10 17:54:21 +0300

    Update missionaries_and_cannibals.py

--- Artificial Intelligence Principles/missionaries_and_cannibals.py
+++ Artificial Intelligence Principles/missionaries_and_cannibals.py
@@ -1,3 +1,16 @@
-missionaries = ['a', 'b', 'c']
-cannibals = ['A', 'B', 'C']
+'''
+Question: In the missionaries and cannibals problem, 
+three missionaries and three cannibals must cross a river using a boat which can carry at most two people, 
+under the constraint that, for both banks, if there are missionaries present on the bank, 
+they cannot be outnumbered by cannibals (if they were, the cannibals would eat the missionaries). 
+The boat cannot cross the river by itself with no people on board.
+'''
+leftMiss = 3
+rightMiss = 0
+
+leftCan = 3
+rightCan = 0
+
+path = []
+
 boat = True
