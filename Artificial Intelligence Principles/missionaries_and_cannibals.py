4c87298a744f8c3e1774f6a4e833e91c4cc5e58e
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-10-10 18:18:15 +0300

    Update missionaries_and_cannibals.py

--- Artificial Intelligence Principles/missionaries_and_cannibals.py
+++ Artificial Intelligence Principles/missionaries_and_cannibals.py
@@ -5,12 +5,30 @@ under the constraint that, for both banks, if there are missionaries present on 
 they cannot be outnumbered by cannibals (if they were, the cannibals would eat the missionaries). 
 The boat cannot cross the river by itself with no people on board.
 '''
-leftMiss = 3
-rightMiss = 0
 
-leftCan = 3
-rightCan = 0
+path = [] #the stack
 
-path = []
+#boat = True, boat could be a boolean with True being the left, False the right
 
-boat = True
+initial_state = (3, 3, 1) #3 missionaries, 3 cannibals and the boat to the left 
+end_state = (0, 0, 0) #0 miss, 0 canns to the left, boat not to the left
+moves = [(1, 0), (0, 1), (2, 0), (0, 2)]
+boat = ["Left", "Right"]
+side = []
+visited = {}
+    
+
+def get_valid_move(self, missionaries, cannibals):
+
+    return (0 <= missionaries <= 3) and (0 <= cannibals <= 3)
+
+def init_state(missionaries, cannibals, side):
+    return (missionaries, cannibals, side) == initial_state
+
+def goal_state(missionaries, cannibals, side):
+    return (missionaries, cannibals, side) == goal_state
+
+
+
+def solve():
+    return 1
