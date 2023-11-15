bebc3a57bcfc8a46868f52b46efb8c5d55b6f2f8
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-11-15 17:55:46 +0200

    Update and rename MyAi.java to greedy.java

--- Artificial Intelligence Principles/AI Final Project/MyAi.java
+++
@@ -1,2 +0,0 @@
-/* Makes wonders
-  */
---
+++ Artificial Intelligence Principles/AI Final Project/greedy.java
@@ -0,0 +1,4 @@
+/* 
+a)	First, you should implement a greedy algorithm for playing the next move of the game. Implement this in the method that returns the next move of the computer. 
+The hardest part of this process is to implement also an appropriate heuristic function for evaluating board positions.  – (15%)
+  */
