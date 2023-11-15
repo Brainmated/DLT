127e41373a7690a81e75d4ff45b80dfc8533fc9b
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-11-15 17:56:37 +0200

    Create minMax.java

---
+++ Artificial Intelligence Principles/AI Final Project/minMax.java
@@ -0,0 +1,4 @@
+/*
+b)	Then extend your previous design by implementing the min max algorithm. You should use a different class in order to preserve both the min-man and greedy methods.  – (15%)
+You can also try implement further improvements like using alpha-beta cutoffs. – (5%)
+*/
