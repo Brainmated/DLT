113d8d1b2a001fff20a1bc39e343a875e054d8f4
Brainmated <alsynetos@hotmail.com>
2023-11-27 17:51:03 +0200

    yes

--- Artificial Intelligence Principles/AI Final Project/SuperTicTacToeAI.java
+++ Artificial Intelligence Principles/AI Final Project/SuperTicTacToeAI.java
@@ -1,36 +1,49 @@
+public class SuperTicTacToeAI extends SuperTicTacToeAI {
+    
+    @Override
+    public int[] nextMove(SuperTicTacToe gs, char maxPlayer) {
+        char[][] board = gs.getBoard();
+        int[] activeBoard = gs.getActiveBoard();
 
-/**
- * You should extend (inherit from) this class and implement your AI
- *
- */
-public class SuperTicTacToeAI {
-	
-	/**
-	 * This method should return the coordinate of the next move (where to place next piece)
-	 * NOTE: It just places in the next possible spot, using no strategy.
-	 * --- INHERIT FROM THIS CLASS AND BUILD THE AI ---
-	 * @param gs The gamestate (i.e. the board, active small board etc)
-	 * @param maxPlayer the char of the current (max) player - for whom you return the move
-	 * @return coordinates as a int[2] array
-	 */
-	public int[] nextMove(SuperTicTacToe gs, char maxPlayer)
-	{
-		char[][] board = gs.getBoard();
-		@SuppressWarnings("unused")
-		int[] activeboard = gs.getActiveBoard();
-		/*
-		for (int y=0; y<SuperTicTacToe.BOARDSIZE; y++)
-		{
-			for (int x=0; x<SuperTicTacToe.BOARDSIZE; x++)
-				System.out.print(board[x][y]);
-			System.out.println();
-		}
-		System.out.println(Arrays.toString(activeboard));
-		*/
-		for (int x=0; x<SuperTicTacToe.BOARDSIZE; x++)
-			for (int y=0; y<SuperTicTacToe.BOARDSIZE; y++)
-				if (gs.inActiveBoard(new int[] {x,y}) && board[x][y]==SuperTicTacToe.SPACE)
-					return new int[]{x,y};
-		return new int[]{0,0}; // should not reach here
-	}
+        int bestScore = Integer.MIN_VALUE;
+        int[] bestMove = null;
+
+        for (int x = 0; x < SuperTicTacToe.BOARDSIZE; x++) {
+            for (int y = 0; y < SuperTicTacToe.BOARDSIZE; y++) {
+                if (gs.inActiveBoard(new int[] { x, y }) && board[x][y] == SuperTicTacToe.SPACE) {
+                    // Simulate making the move
+                    char[][] newBoard = copyBoard(board);
+                    newBoard[x][y] = maxPlayer;
+
+                    // Evaluate the board position using the heuristic function
+                    int score = evaluatePosition(newBoard, maxPlayer);
+
+                    // Update the best move if the current move has a higher score
+                    if (score > bestScore) {
+                        bestScore = score;
+                        bestMove = new int[] { x, y };
+                    }
+                }
+            }
+        }
+
+        if (bestMove != null) {
+            return bestMove;
+        } else {
+            return new int[] { 0, 0 }; // Default move
+        }
+    }
+
+    private char[][] copyBoard(char[][] board) {
+        char[][] newBoard = new char[SuperTicTacToe.BOARDSIZE][SuperTicTacToe.BOARDSIZE];
+        for (int i = 0; i < SuperTicTacToe.BOARDSIZE; i++) {
+            System.arraycopy(board[i], 0, newBoard[i], 0, SuperTicTacToe.BOARDSIZE);
+        }
+        return newBoard;
+    }
+
+    private int evaluatePosition(char[][] board, char player) {
+
+        return 0;
+    }
 }
