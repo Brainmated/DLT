79a38b0d4f254302769747b2b8507b06c412ce8a
Brainmated <alsynetos@hotmail.com>
2023-11-27 20:19:10 +0200

    New

--- Artificial Intelligence Principles/AI Final Project/SuperTicTacToeAI.java
+++ Artificial Intelligence Principles/AI Final Project/SuperTicTacToeAI.java
@@ -12,23 +12,43 @@ public class SuperTicTacToeAI {
 	 * @return coordinates as a int[2] array
 	 */
 	public int[] nextMove(SuperTicTacToe gs, char maxPlayer)
-	{
-		char[][] board = gs.getBoard();
-		@SuppressWarnings("unused")
-		int[] activeboard = gs.getActiveBoard();
-		/*
-		for (int y=0; y<SuperTicTacToe.BOARDSIZE; y++)
-		{
-			for (int x=0; x<SuperTicTacToe.BOARDSIZE; x++)
-				System.out.print(board[x][y]);
-			System.out.println();
-		}
-		System.out.println(Arrays.toString(activeboard));
-		*/
-		for (int x=0; x<SuperTicTacToe.BOARDSIZE; x++)
-			for (int y=0; y<SuperTicTacToe.BOARDSIZE; y++)
-				if (gs.inActiveBoard(new int[] {x,y}) && board[x][y]==SuperTicTacToe.SPACE)
-					return new int[]{x,y};
-		return new int[]{0,0}; // should not reach here
-	}
+{
+    char[][] board = gs.getBoard();
+    int[] activeboard = gs.getActiveBoard();
+
+    // Greedy heuristic: Try to win, block opponent's win, or create an "open two".
+    for (int x=0; x<SuperTicTacToe.BOARDSIZE; x++) {
+        for (int y=0; y<SuperTicTacToe.BOARDSIZE; y++) {
+            if (gs.inActiveBoard(new int[] {x,y}) && board[x][y]==SuperTicTacToe.SPACE) {
+                // Check if this move would win the game for the AI.
+                board[x][y] = maxPlayer;
+                if (gs.isGameFinished()) {
+                    board[x][y] = SuperTicTacToe.SPACE;  // undo move
+                    return new int[]{x,y};
+                }
+                // Check if this move would block the human from winning.
+                board[x][y] = (maxPlayer == SuperTicTacToe.P1) ? SuperTicTacToe.P2 : SuperTicTacToe.P1;
+                if (gs.isGameFinished()) {
+                    board[x][y] = SuperTicTacToe.SPACE;  // undo move
+                    return new int[]{x,y};
+                }
+                // Check for "open two" opportunities.
+                // TODO: You will need to implement this yourself based on your game rules.
+                // If an "open two" opportunity is found, return new int[]{x,y}.
+
+                board[x][y] = SuperTicTacToe.SPACE;  // undo move
+            }
+        }
+    }
+
+    // If no strategic move is found, just return the first available move.
+    for (int x=0; x<SuperTicTacToe.BOARDSIZE; x++) {
+        for (int y=0; y<SuperTicTacToe.BOARDSIZE; y++) {
+            if (gs.inActiveBoard(new int[] {x,y}) && board[x][y]==SuperTicTacToe.SPACE) {
+                return new int[]{x,y};
+            }
+        }
+    }
+
+    return new int[]{0,0}; // should not reach here
 }
