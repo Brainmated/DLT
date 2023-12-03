697b37c12763a554081c4958fb6b0309f30c68ae
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-12-03 20:05:09 +0200

    Add files via upload

--- Artificial Intelligence Principles/AI Final Project/minMax.java
+++ Artificial Intelligence Principles/AI Final Project/minMax.java
@@ -4,29 +4,29 @@ public class minMax extends SuperTicTacToeAI {
     final char aiPlayer = 'O';
     final char humanPlayer = 'X';
 
-    // Constructor to initialize the game
+    //constructor to initialize the game
     public minMax(SuperTicTacToe game) {
         this.game = game;
     }
 
-    // Function to get the best possible move for the AI player
+    //function to get the best possible move for the AI player
     public int[] getBestMove(char[][] board) {
         int bestScore = Integer.MIN_VALUE;
         int[] bestMove = new int[2];
 
-        // Iterate over all squares on the board
+        //iterate over all squares on the board
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                // Check if the current move is valid
+                //check if the current move is valid
                 if (game.checkMoveValidity(new int[] {i, j}, aiPlayer, board)) {
-                    // Make a copy of the board and apply the move
+                    //make a copy of the board and apply the move
                     char[][] copy = copyBoard(board);
                     copy[i][j] = aiPlayer;
                     
-                    // Use the Minimax algorithm to find the score of the current move
+                    //use the Minimax algorithm to find the score of the current move
                     int score = minimax(copy, 0, false, Integer.MIN_VALUE, Integer.MAX_VALUE);
 
-                    // If the current score is better than the best score, update the best score and best move
+                    //if the current score is better than the best score, update the best score and best move
                     if (score > bestScore) {
                         bestScore = score;
                         bestMove[0] = i;
@@ -36,51 +36,51 @@ public class minMax extends SuperTicTacToeAI {
             }
         }
 
-        // Return the best move
+        //return the best move
         return bestMove;
     }
 
-    // The Minimax function, which also implements alpha-beta pruning
+    //the Minimax function, which also implements alpha-beta pruning
     public int minimax(char[][] board, int depth, boolean isMaximizing, int alpha, int beta) {
-        // Check if the game has ended
+        //check if the game has ended
         if (game.isGameFinished()) {
             float sum = 0f;
-            // Calculate the total score
+            //calculate the total score
             for (int x = 0; x < game.NOSQUARESXY; x++) {
                 for (int y = 0; y < game.NOSQUARESXY; y++) {
                     sum += game.pointsWon(new int[] {x, y}, board);
                 }
             }
 
-            // Check who won and return the corresponding score
+            //check who won and return the corresponding score
             if (sum > 0.4f) {
-                return 1; // AI player won
+                return 1; //aI player won
             } else if (sum < -0.4f) {
-                return -1; // human player won
+                return -1; //minmax won
             } else {
-                return 0; // tie
+                return 0; //tie
             }
         }
 
-        // If it's the AI player's turn (Maximizing player)
+        //if it's the AI player's turn (Maximizing player)
         if (isMaximizing) {
             int bestScore = Integer.MIN_VALUE;
             
-            // Iterate over all squares on the board 
+            //iterate over all squares on the board 
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[i].length; j++) {
-                    // Check if the square is empty
+                    //check if the square is empty
                     if (board[i][j] == '\0') {
-                        // Try this move for the AI player
+                        //try this move for the AI player
                         board[i][j] = aiPlayer;
                         
-                        // Call the Minimax function recursively and choose the maximum score
+                        //call the Minimax function recursively and choose the maximum score
                         int score = minimax(board, depth + 1, false, alpha, beta);
                         board[i][j] = '\0';
                         bestScore = Math.max(score, bestScore);
                         alpha = Math.max(alpha, score);
 
-                        // Alpha Beta Pruning
+                        //alpha Beta Pruning
                         if (beta <= alpha) {
                             return bestScore;
                         }
@@ -89,25 +89,25 @@ public class minMax extends SuperTicTacToeAI {
             }
             return bestScore;
 
-        // If it's the human player's turn (Minimizing player)
+        //if it's the other player's turn (Minimizing player)
         } else {
             int bestScore = Integer.MAX_VALUE;
             
-            // Iterate over all squares on the board 
+            //iterate over all squares on the board 
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[i].length; j++) {
-                    // Check if the square is empty
+                    //check if the square is empty
                     if (board[i][j] == '\0') {
-                        // Try this move for the human player
+                        //try this move for the human player
                         board[i][j] = humanPlayer;
                         
-                        // Call the Minimax function recursively and choose the minimum score
+                        //call the Minimax function recursively and choose the minimum score
                         int score = minimax(board, depth + 1, true, alpha, beta);
                         board[i][j] = '\0';
                         bestScore = Math.min(score, bestScore);
                         beta = Math.min(beta, score);
 
-                        // Alpha Beta Pruning
+                        //alpha Beta Pruning
                         if (beta <= alpha) {
                             return bestScore;
                         }
@@ -118,7 +118,7 @@ public class minMax extends SuperTicTacToeAI {
         }
     }
 
-    // Override the nextMove function to use Minimax AI
+    //override the nextMove function to use Minimax AI
     @Override
     public int[] nextMove(SuperTicTacToe gs, char maxPlayer) {
         return getBestMove(gs.getBoard());
