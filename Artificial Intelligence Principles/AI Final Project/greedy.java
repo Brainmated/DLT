d2dbaf9a85b6b85e3af17d78ea61ac30f91012bd
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2023-12-03 20:06:28 +0200

    Add files via upload

--- Artificial Intelligence Principles/AI Final Project/greedy.java
+++ Artificial Intelligence Principles/AI Final Project/greedy.java
@@ -1,91 +1,58 @@
-/**
- * Greedy AI for Super Tic Tac Toe game.
- * This AI evaluates all possible moves and chooses the one that maximizes its own presence on the board.
- * It does not consider the opponent's potential moves.
- * 
- * @author Alexandros Synetos Konstantinidis
- */
-public class Greedy extends SuperTicTacToeAI {
-
-    private SuperTicTacToe game;  // The instance of the game this AI is playing
-    final char aiPlayer = 'X';  // The character representing the AI player on the game board
-
-    /**
-     * Constructor that initializes the game instance this AI is playing.
-     *
-     * @param game the SuperTicTacToe game instance.
-     */
-    public Greedy(SuperTicTacToe game) {
+public class greedy extends SuperTicTacToeAI{
+    private SuperTicTacToe game;
+    final char aiPlayer = 'X';
+    public greedy(SuperTicTacToe game) {
         this.game = game;
     }
-
-    /**
-     * Evaluates all possible moves and returns the best one according to the evaluateBoard method.
-     *
-     * @param board the game board to evaluate.
-     * @return the coordinates of the best move.
-     */
+    
     public int[] getBestMove(char[][] board) {
-        int bestScore = Integer.MIN_VALUE;  // Initialize the best score to the lowest possible integer
-        int[] bestMove = new int[2];  // Array to store the best move
+        int bestScore = Integer.MIN_VALUE;
+        int[] bestMove = new int[2]; //store the best move
 
-        // Iterate over all cells of the board
+        //iterate through all cells on the board
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                // If the move is valid
+                //check if the move is valid
                 if (game.checkMoveValidity(new int[] {i, j}, aiPlayer, board)) {
-                    // Make a copy of the board
+                    //make a copy of the board
                     char[][] copy = copyBoard(board);
-                    // Simulate a move on the copied board
+                    //simulate a move on the copied board
                     copy[i][j] = aiPlayer; 
-                    // Evaluate the board state after the move
+                    //evaluate the board state after the move
                     int score = evaluateBoard(copy);
-
-                    // If this score is better than the current best score, update best score and best move
+                    System.out.printf("Move [%d,%d] has score %d\n", i, j, score);
+                    //if this score is better than the current best score, update best score and best move
                     if (score > bestScore) {
                         bestScore = score;
                         bestMove[0] = i;
                         bestMove[1] = j;
+                        System.out.printf("Best move so far is [%d,%d] with score %d\n", i, j, bestScore);
                     }
                 }
             }
         }
 
-        return bestMove;  // Return the best move found
+        return bestMove;
     }
-
-    /**
-     * Evaluates a board state by counting the number of cells occupied by 'X'.
-     *
-     * @param board the game board to evaluate.
-     * @return the score of the board.
-     */
+    
     public int evaluateBoard(char[][] board) {
-        int score = 0;  // Initialize score
+        int score = 0;
 
-        // Iterate over all cells of the board
+        //prefer boards where 'X' has more marks
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                // Increment score if cell is occupied by 'X'
                 if (board[i][j] == 'X') {
                     score++;
                 }
             }
         }
 
-        return score;  // Return the final score
+        return score;
     }
-
-    /**
-     * Returns the next move this AI will make. 
-     * Implements the required method from the SuperTicTacToeAI abstract class.
-     *
-     * @param gs the current game state.
-     * @param maxPlayer the character representing the AI player on the board.
-     * @return the coordinates of the next move.
-     */
+    
     @Override
     public int[] nextMove(SuperTicTacToe gs, char maxPlayer) {
         return getBestMove(gs.getBoard());
     }
+    
 }
